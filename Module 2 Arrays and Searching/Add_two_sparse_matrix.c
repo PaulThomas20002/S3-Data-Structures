c821be20518062478b919ef56a3a8bfd9364b18c
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-11-28 11:04:09 +0530

    not yet completed some logical error found

--- Module 2 Arrays and Searching/Add_two_sparse_matrix.c
+++ Module 2 Arrays and Searching/Add_two_sparse_matrix.c
@@ -1,6 +1,11 @@
 /* logical error found, not completed */
-  include<stdio.h>
-int main(){
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+
+{
 int i=1,j=1,k=1,m ,n, a[10][3], b[10][3] , c[10][3];
 
 printf("Enter the num of rows of matrix A");
@@ -14,7 +19,7 @@ for(i=0; i<m;i++)
 printf("Enter the num of rows of matrix B");
 scanf("%d", &n);
 printf("Enter the matrix B");
-for(i=0; i<m;i++)
+for(i=0; i<n;i++)
 	for(j=0;j<3;j++)
 	    {
 		    scanf("%d", &b[i][j]);
@@ -33,10 +38,10 @@ if( a[0][0] == b[0][0] && a[0][1] == b[0][1] )
 			c[k][2]=a[i][2]+b[i][2];
 			i++;j++;k++;
 		}
-		else if( a[i][0]==a[j][0])
+		else if( a[i][0]==b[j][0])
 		   {
 			
-			if(a[i][1]<b[i][1])
+			if(a[i][1]<b[j][1])
 			    {
 				c[k][0]=a[i][0];
 				c[k][1]=a[i][1];
@@ -53,7 +58,7 @@ if( a[0][0] == b[0][0] && a[0][1] == b[0][1] )
 					
 		else
 		{
-			if(a[i][0]<b[i][0])
+			if(a[i][0]<b[j][0])
 			{
 				c[k][0]=a[i][0];
 				c[k][1]=a[i][1];
@@ -86,22 +91,23 @@ if( a[0][0] == b[0][0] && a[0][1] == b[0][1] )
 		
 	c[0][0]=a[0][0];
 	c[0][1]=a[0][1];
-	c[0][2]=k-1	;
+	c[0][2]=k-1;
 	}
 
 }
 else
     {
         printf("\nADDITION NOT POSSIBLE");
+        exit(0);
     }
        
 
 printf("The matrix A+B");
 printf("\nrow\tcolumn\tvalue\n");
-for(i=1; i<k; i++)
+for(i=0; i<k; i++)
 	
 	    {
 		    printf("\n %d \t %d \t %d", c[i][0], c[i][1], c[i][2]);
 		}
-return 0;
+		return 0;
 }
