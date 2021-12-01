20c8b3ddd4c249ac8cb6e6289e68afd08b6d0ff4
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-01 06:04:06 +0530

    add algorithm to prgm

--- Module 2 Arrays and Searching/binarysort2.c
+++ Module 2 Arrays and Searching/binarysort2.c
@@ -1,3 +1,17 @@
+/*
+algorithm for binary sort
+step 1: start
+step 2: accept a value in max as the number of elements of the array
+step 3: accept max elements into the array ar
+step 4: accept the value to be searched in the variable item
+step 5 : store the postion of the first element of the list in first in first anf that of the last in the last
+step 6 : repeat steps 7 to 11 while(first<=last)
+step 7 : find the middle position using the formula (first+last)/2 and store it in mid
+step 8 : compare the search value in item with the elements at the mid of the list
+step 9 : if the mid element contains the search value in the item then stop search, display the position and go to step 12
+step 10 : if the value is smaller than the mid element'THEN set last = mid-1
+step 11 : if the search value is larger than the mid element    THEN    set first = mid+1
+*/
 #include<stdio.h>
 int main(){
     int ar[25], max,first, last, mid, i, item , loc=-1;
