85d6441d09b80b119ba9263cd6960538d22ded29
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2021-12-07 21:41:47 +0530

    Update convert_a_matrix_to_transpose_of_its_sparse.c

--- Module 2 Arrays and Searching/CSL201-DS-Lab/convert_a_matrix_to_transpose_of_its_sparse.c
+++ Module 2 Arrays and Searching/CSL201-DS-Lab/convert_a_matrix_to_transpose_of_its_sparse.c
@@ -1,61 +1,62 @@
-#include<stdio.h>
-void main(){
-int m,p,n,i,j,k,t=0,l,a[10][10],s[10][10],st[10][10];
-printf("Enter the size of the orginal array");
-scanf("%d%d",&m,&n);
-printf("\nEnter the array elements\n");
-for(i=0;i<m;i++){
-  for(j=0;j<n;j++){
-    scanf("%d",&a[i][j]);
-                  }
-                }
-k=1;
-for(i=0;i<m;i++){
-  for(j=0;j<n;j++){
-    if(a[i][j]!=0){
-        s[k][0]=i;
-        s[k][1]=j;
-        s[k][2]=a[i][j];
-        k++;
-        if(j>t)
-        t=j;
-                }
-                }
-              }
-s[0][0]=m;
-s[0][1]=n;
-s[0][2]=k-1;
-printf("3 tuple representation of the given matrix is\n");
-for(i=0;i<k;i++){
-  printf("\n");
-  for(j=0;j<3;j++){
-    printf(" %d",s[i][j]);
-                  }
-                }
-p=1;
-l=k-1;
-printf("\nt= %d, l= %d",t,l);
-for(i=0;i<=t;i++){
-  k=1;
-  for(j=0;j<l;j++){
-      if(s[k][1]==i){
-          st[p][1]=s[k][0];
-          st[p][0]=s[k][1];
-          st[p][2]=s[k][2];
-          p++;  k++;
-                    }
-    else
-    k++;
-                  }
-                }
-st[0][0]=s[0][1];
-st[0][1]=s[0][0];
-st[0][2]=s[0][2];
-printf("\nTranspose of the sparse matrix is\n");
-for(i=0;i<p;i++){
-  printf("\n");
-  for(j=0;j<3;j++){
-  printf(" %d",st[i][j]);
-                  }
-                }
+#include <stdio.h>
+int main(){
+    int b[10][10], b1[10][10], b2[10][10], i, j, k, t=0, m, n, p, l; 
+    printf("Enter the rows and cols : ");
+    scanf("%d%d", &m, &n);
+    printf("Enter the matrix");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf("%d", &b[i][j]);
+        }
+    }
+    k=1;
+    for(i=0; i<m; i++){
+        for(j=0;j<n; j++){
+            if(b[i][j]!=0){
+                b1[k][0]=i;
+                b1[k][1]=j;
+                b1[k][2]=b[i][j];
+                k++;
+                if(j>t)
+                    t=j;
+            }
+        }
+    }
+    b1[0][0]=m;
+    b1[0][1]=n;
+    b1[0][2]=k-1;
+    
+    printf("Sparse matrix is\n");
+    for(i=0; i<k; i++){
+        printf("%d\t%d\t%d\n", b1[i][0], b1[i][1], b1[i][2]);
+    }
+    
+    //upto sparse matrix set ayyiittind
+    
+   p=1;
+   l=k-1;
+   printf("Non zero = %d\n", l);
+   
+   for(i=0; i<=t; i++){
+       k=1;
+       for(j=0; j<l; j++){
+           if(b1[k][1]==i){
+               b2[p][0]=b1[k][1];
+               b2[p][1]=b1[k][0];
+               b2[p][2]=b1[k][2];
+               k++;p++;
+           }
+           else
+            k++;
+       }
+   }
+   b2[0][0]=b1[0][1];
+   b2[0][1]=b1[0][0];
+   b2[0][2]=b1[0][2];
+   
+   printf("Transpose of Sparse matrix is\n");
+    for(i=0; i<k; i++){
+        printf("%d\t%d\t%d\n", b2[i][0], b2[i][1], b2[i][2]);
+    }
+    return 0;
 }
